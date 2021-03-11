0ac5312e01087d6b3a6247d917da6e052e372f5d
AnaMariaMoga <anamoga435@gmail.com>
2021-03-11 17:42:45 +0200

    feat: add image and snapshot interfaces

---
+++ pkg/resources/image.go
@@ -0,0 +1,58 @@
+package resources
+
+import (
+	"context"
+
+	ionoscloud "github.com/ionos-cloud/sdk-go/v5"
+)
+
+type Image struct {
+	ionoscloud.Image
+}
+
+type Images struct {
+	ionoscloud.Images
+}
+
+type ImageProperties struct {
+	ionoscloud.ImageProperties
+}
+
+// ImagesService is a wrapper around ionoscloud.Image
+type ImagesService interface {
+	List() (Images, *Response, error)
+	Get(imageId string) (*Image, *Response, error)
+	Delete(imageId string) (*Response, error)
+}
+
+type imagesService struct {
+	client  *Client
+	context context.Context
+}
+
+var _ ImagesService = &imagesService{}
+
+func NewImageService(client *Client, ctx context.Context) ImagesService {
+	return &imagesService{
+		client:  client,
+		context: ctx,
+	}
+}
+
+func (s *imagesService) List() (Images, *Response, error) {
+	req := s.client.ImageApi.ImagesGet(s.context)
+	images, resp, err := s.client.ImageApi.ImagesGetExecute(req)
+	return Images{images}, &Response{*resp}, err
+}
+
+func (s *imagesService) Get(imageId string) (*Image, *Response, error) {
+	req := s.client.ImageApi.ImagesFindById(s.context, imageId)
+	image, resp, err := s.client.ImageApi.ImagesFindByIdExecute(req)
+	return &Image{image}, &Response{*resp}, err
+}
+
+func (s *imagesService) Delete(imageId string) (*Response, error) {
+	req := s.client.ImageApi.ImagesDelete(s.context, imageId)
+	_, resp, err := s.client.ImageApi.ImagesDeleteExecute(req)
+	return &Response{*resp}, err
+}
---
+++ pkg/resources/snapshot.go
@@ -0,0 +1,88 @@
+package resources
+
+import (
+	"context"
+
+	ionoscloud "github.com/ionos-cloud/sdk-go/v5"
+)
+
+type Snapshot struct {
+	ionoscloud.Snapshot
+}
+
+type Snapshots struct {
+	ionoscloud.Snapshots
+}
+
+type SnapshotProperties struct {
+	ionoscloud.SnapshotProperties
+}
+
+// SnapshotsService is a wrapper around ionoscloud.Snapshot
+type SnapshotsService interface {
+	List() (Snapshots, *Response, error)
+	Get(snapshotId string) (*Snapshot, *Response, error)
+	Create(datacenterId, volumeId, name, description, licenceType string) (*Snapshot, *Response, error)
+	Update(snapshotId string, snapshotProp SnapshotProperties) (*Snapshot, *Response, error)
+	Restore(datacenterId, volumeId string) (*Response, error)
+	Delete(snapshotId string) (*Response, error)
+}
+
+type snapshotsService struct {
+	client  *Client
+	context context.Context
+}
+
+var _ SnapshotsService = &snapshotsService{}
+
+func NewSnapshotService(client *Client, ctx context.Context) SnapshotsService {
+	return &snapshotsService{
+		client:  client,
+		context: ctx,
+	}
+}
+
+func (s *snapshotsService) List() (Snapshots, *Response, error) {
+	req := s.client.SnapshotApi.SnapshotsGet(s.context)
+	snapshots, resp, err := s.client.SnapshotApi.SnapshotsGetExecute(req)
+	return Snapshots{snapshots}, &Response{*resp}, err
+}
+
+func (s *snapshotsService) Get(snapshotId string) (*Snapshot, *Response, error) {
+	req := s.client.SnapshotApi.SnapshotsFindById(s.context, snapshotId)
+	snapshot, resp, err := s.client.SnapshotApi.SnapshotsFindByIdExecute(req)
+	return &Snapshot{snapshot}, &Response{*resp}, err
+}
+
+func (s *snapshotsService) Create(datacenterId, volumeId, name, description, licenceType string) (*Snapshot, *Response, error) {
+	req := s.client.VolumeApi.DatacentersVolumesCreateSnapshotPost(s.context, datacenterId, volumeId)
+	if name != "" {
+		req = req.Name(name)
+	}
+	if description != "" {
+		req = req.Description(description)
+	}
+	if licenceType != "" {
+		req = req.LicenceType(licenceType)
+	}
+	snapshot, resp, err := s.client.VolumeApi.DatacentersVolumesCreateSnapshotPostExecute(req)
+	return &Snapshot{snapshot}, &Response{*resp}, err
+}
+
+func (s *snapshotsService) Update(snapshotId string, snapshotProp SnapshotProperties) (*Snapshot, *Response, error) {
+	req := s.client.SnapshotApi.SnapshotsPatch(s.context, snapshotId).Snapshot(snapshotProp.SnapshotProperties)
+	snapshot, resp, err := s.client.SnapshotApi.SnapshotsPatchExecute(req)
+	return &Snapshot{snapshot}, &Response{*resp}, err
+}
+
+func (s *snapshotsService) Restore(datacenterId, volumeId string) (*Response, error) {
+	req := s.client.VolumeApi.DatacentersVolumesRestoreSnapshotPost(s.context, datacenterId, volumeId)
+	_, resp, err := s.client.VolumeApi.DatacentersVolumesRestoreSnapshotPostExecute(req)
+	return &Response{*resp}, err
+}
+
+func (s *snapshotsService) Delete(snapshotId string) (*Response, error) {
+	req := s.client.SnapshotApi.SnapshotsDelete(s.context, snapshotId)
+	_, resp, err := s.client.SnapshotApi.SnapshotsDeleteExecute(req)
+	return &Response{*resp}, err
+}
