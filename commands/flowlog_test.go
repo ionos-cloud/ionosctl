2b9426a1e17a86294b8ef1f4b0bc32c1dde41a06
Gabriel Savu <gabriel-catalin.savu@ionos.com>
2021-07-06 16:41:13 +0300

    tests fixed

--- commands/flowlog_test.go
+++ commands/flowlog_test.go
@@ -335,9 +335,13 @@ func TestGetFlowLogsIds(t *testing.T) {
 	var b bytes.Buffer
 	clierror.ErrAction = func() {}
 	w := bufio.NewWriter(&b)
-	viper.Set(config.ArgConfig, "../pkg/testdata/config.json")
+	err := os.Setenv(ionoscloud.IonosUsernameEnvVar, "user")
+	assert.NoError(t, err)
+	err = os.Setenv(ionoscloud.IonosPasswordEnvVar, "pass")
+	assert.NoError(t, err)
+	viper.Set(config.ArgServerUrl, config.DefaultApiURL)
 	getFlowLogsIds(w, testFlowLogVar, testFlowLogVar, testFlowLogVar)
-	err := w.Flush()
+	err = w.Flush()
 	assert.NoError(t, err)
 	re := regexp.MustCompile(`401 Unauthorized`)
 	assert.True(t, re.Match(b.Bytes()))
