6cac86a2a5f6f0e58808b92ffe19ae3b7e523bce
AnaMariaMoga <59568286+AnaMariaMoga@users.noreply.github.com>
2021-09-30 16:18:38 +0300

    Fix: ionosctl version (#114)

--- tools/build.sh
+++ tools/build.sh
@@ -8,13 +8,7 @@ mkdir -p "${OUT_D}"
 
 version="$(git tag -l | sort --version-sort | tail -n1 | cut -c 2-)"
 
-major="$(echo "$version" | cut -d . -f1)"
-minor="$(echo "$version" | cut -d . -f2)"
-patch="$(echo "$version" | cut -d . -f3)"
-
-ldflags="-X github.com/ionos-cloud/ionosctl/commands.Major=${major}"
-ldflags="${ldflags} -X github.com/ionos-cloud/ionosctl/commands.Minor=${minor}"
-ldflags="${ldflags} -X github.com/ionos-cloud/ionosctl/commands.Patch=${patch}"
+ldflags="-X github.com/ionos-cloud/ionosctl/commands.Version=${version}"
 
 (
     export GO111MODULE=on
