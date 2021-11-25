6b88cf4564391641897c441bd1cbd17c52f7c18f
AnaMariaMoga <anamoga435@gmail.com>
2021-11-25 12:48:43 +0200

    feat: add setup

---
+++ commands/auth-v1/examples.go
@@ -0,0 +1,14 @@
+package authv1
+
+const (
+	listTokenExample     = `ionosctl token list`
+	getTokenExample      = `ionosctl token get --token-id TOKEN_ID`
+	generateTokenExample = `ionosctl token generate`
+	deleteTokenExample   = `ionosctl token delete --token-id TOKEN_ID
+
+ionosctl token delete --expired
+
+ionosctl token delete --current
+
+ionosctl token delete --all`
+)
