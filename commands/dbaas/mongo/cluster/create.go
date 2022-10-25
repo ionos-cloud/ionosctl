5708fcdef863b6a4ddf9ba4fd6bb136c1a0c31bd
Alexandru Claudius Virtopeanu <alexandru-claudius.virtopeanu@ionos.com>
2022-10-25 17:52:24 +0300

    link flags to a struct

--- commands/dbaas/mongo/cluster/create.go
+++ commands/dbaas/mongo/cluster/create.go
@@ -13,6 +13,7 @@ import (
 	"github.com/ionos-cloud/ionosctl/v6/pkg/core"
 	ionoscloud "github.com/ionos-cloud/sdk-go-dbaas-mongo"
 	"github.com/spf13/cobra"
+	"os"
 )
 
 var createProperties = ionoscloud.CreateClusterProperties{}
