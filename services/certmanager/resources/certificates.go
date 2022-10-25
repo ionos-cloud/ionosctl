48b7e8d1c182e7c596a1fb56643d0cfc44dd7c2d
dgheorghioiu-ionos <dragos.gheorghioiu@ionos.com>
2022-10-26 02:00:16 +0300

    starting implementation of get certificate by id

---
+++ services/certmanager/resources/certificates.go
@@ -0,0 +1,35 @@
+package resources
+
+import (
+	"context"
+	sdkgo "github.com/ionos-cloud/sdk-go-cert-manager"
+)
+
+type Response struct {
+	sdkgo.APIResponse
+}
+
+// CertsService is a wrapper around ionoscloud.CertificateDto
+type CertsService interface {
+	Get(certId string) (sdkgo.CertificateDto, *sdkgo.APIResponse, error)
+}
+
+type certsService struct {
+	client  *Client
+	context context.Context
+}
+
+var _ CertsService = &certsService{}
+
+func NewCertsService(client *Client, ctx context.Context) CertsService {
+	return &certsService{
+		client:  client,
+		context: ctx,
+	}
+}
+
+func (svc *certsService) Get(certId string) (sdkgo.CertificateDto, *sdkgo.APIResponse, error) {
+	req := svc.client.CertificatesApi.CertificatesGetById(svc.context, certId)
+	cert, res, err := svc.client.CertificatesApi.CertificatesGetByIdExecute(req)
+	return cert, res, err
+}
---
+++ services/certmanager/services.go
@@ -0,0 +1,34 @@
+package certmanager
+
+import (
+	"context"
+	"github.com/ionos-cloud/ionosctl/pkg/config"
+	"github.com/ionos-cloud/ionosctl/services/certmanager/resources"
+	"github.com/spf13/viper"
+)
+
+type Services struct {
+	// Certificate Manager Resources Services
+	Certs func() resources.CertsService
+	Context context.Context
+}
+
+// InitClient for Commands
+func (c *Services) InitClient() (*resources.Client, error) {
+	clientSvc, err := resources.NewClientService(
+		viper.GetString(config.Username),
+		viper.GetString(config.Password),
+		viper.GetString(config.Token), // Token support
+		config.GetServerUrl(),
+	)
+	if err != nil {
+		return nil, err
+	}
+	return clientSvc.Get(), nil
+}
+
+// InitServices for Commands
+func (c *Services) InitServices(client *resources.Client) error {
+	c.Certs = func() resources.CertsService { return resources.NewCertsService(client, c.Context) }
+	return nil
+}
