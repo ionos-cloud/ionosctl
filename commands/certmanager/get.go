4c9aac6c460d7ef342895a41bfdbb1d96028e29f
dgheorghioiu-ionos <dragos.gheorghioiu@ionos.com>
2022-10-26 17:35:20 +0300

    get first implementation

---
+++ commands/certmanager/get.go
@@ -0,0 +1,48 @@
+package certmanager
+
+import (
+	"context"
+
+	"github.com/ionos-cloud/ionosctl/pkg/config"
+	"github.com/ionos-cloud/ionosctl/pkg/core"
+	"github.com/spf13/cobra"
+)
+
+func CertGetCmd() *core.Command {
+	cmd := core.NewCommand(context.TODO(), nil, core.CommandBuilder{
+		Namespace: "certmanager",
+		Resource:  "certificates",
+		Verb:      "get",
+		Aliases:   []string{"g"},
+		ShortDesc: "Get Certificate by ID",
+		LongDesc:  "Use this command to retrieve a  Certificate by ID.",
+		Example:   "ionosctl certificate-manager get --certificate-id 12345678-1234-1234-1234-123456789012",
+		PreCmdRun: func(c *core.PreCommandConfig) error {
+			err := c.Command.Command.MarkFlagRequired("certificate-id")
+			if err != nil {
+				return err
+			}
+			return nil
+		},
+		CmdRun: func(c *core.CommandConfig) error {
+			c.Printer.Verbose("Getting Clusters...")
+			id, err := c.Command.Command.Flags().GetString("certificate-id")
+			if err != nil {
+				return err
+			}
+			cert, r, err := c.CertificateManagerServices.Certs().GetById(id)
+			if err != nil {
+				return err
+			}
+			return c.Printer.Print(getCertPrint(r, c, &cert))
+		},
+		InitClient: true,
+	})
+
+	cmd.AddStringFlag("certificate-id", "", "", "Response get a single certificate (required)")
+	_ = cmd.Command.RegisterFlagCompletionFunc(config.ArgCols, func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+		return allCols, cobra.ShellCompDirectiveNoFileComp
+	})
+
+	return cmd
+}
