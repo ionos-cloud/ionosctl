f86845bdabf4834d13a9bf2a81c7b2f933fd1d7c
dgheorghioiu-ionos <dragos.gheorghioiu@ionos.com>
2022-11-14 12:16:39 +0200

    removed unused constant

--- commands/certmanager/constants.go
+++ commands/certmanager/constants.go
@@ -13,13 +13,3 @@ const (
 
 	UsageCert = "ionosctl certificate-manager create --name, --cert/--cert-path, --cert-chain/--cert-chain-path, --private-key/--private-key-path"
 )
-
-var RequiredFlagSets = [16][]string{{FlagCertName, FlagCert, FlagCertChain, FlagPrivateKey},
-	{FlagCertName, FlagCertPath, FlagCertChain, FlagPrivateKey},
-	{FlagCertName, FlagCert, FlagCertChainPath, FlagPrivateKey},
-	{FlagCertName, FlagCertPath, FlagCertChainPath, FlagPrivateKey},
-	{FlagCertName, FlagCert, FlagCertChain, FlagPrivateKeyPath},
-	{FlagCertName, FlagCertPath, FlagCertChain, FlagPrivateKeyPath},
-	{FlagCertName, FlagCert, FlagCertChainPath, FlagPrivateKeyPath},
-	{FlagCertName, FlagCertPath, FlagCertChainPath, FlagPrivateKeyPath},
-}
