6846796b0993a62c84e84738d455b398db9d50db
Gheorghioiu Dragos <115638797+dgheorghioiu-ionos@users.noreply.github.com>
2022-12-07 11:34:21 +0200

    fix: 100 CPU usage (#223)
    * changed pollTime
    
    * stopped ticker
    
    * stopped ticker for deletion progress

--- pkg/utils/watch.go
+++ pkg/utils/watch.go
@@ -31,6 +31,7 @@ func WatchStateProgress(ctx context.Context, c *core.CommandConfig, interrogator
 		defer close(errChan)
 		defer close(progressChan)
 		ticker := time.NewTicker(pollTime)
+		defer ticker.Stop()
 		sendingProgress := func(p int) {
 			select {
 			case progressChan <- p:
@@ -88,6 +89,7 @@ func WatchRequestProgress(ctx context.Context, c *core.CommandConfig, interrogat
 		defer close(errChan)
 		defer close(progressChan)
 		ticker := time.NewTicker(pollTime)
+		defer ticker.Stop()
 		sendingProgress := func(p int) {
 			select {
 			case progressChan <- p:
@@ -141,6 +143,7 @@ func WatchDeletionProgress(ctx context.Context, c *core.CommandConfig, interroga
 		defer close(errChan)
 		defer close(progressChan)
 		ticker := time.NewTicker(pollTime)
+		defer ticker.Stop()
 		sendingProgress := func(p int) {
 			select {
 			case progressChan <- p:
