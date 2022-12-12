926d5c5dfe8b9c7077459321993ae08b12d3eb8c
dgheorghioiu-ionos <dragos.gheorghioiu@ionos.com>
2022-12-12 14:09:19 +0200

    create test

--- commands/certmanager/certmanager_test.go
+++ commands/certmanager/certmanager_test.go
@@ -4,13 +4,14 @@ import (
 	"errors"
 	"testing"
 
-	"github.com/ionos-cloud/ionosctl/pkg/core"
+	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
+
+	"github.com/ionos-cloud/ionosctl/pkg/constants"
 )
 
 func TestCertificateManagerServiceCmd(t *testing.T) {
 	var err error
-	core.RootCmdTest.AddCommand(CertCmd())
 	if ok := CertCmd().IsAvailableCommand(); !ok {
 		err = errors.New("non-available cmd")
 	}
@@ -41,8 +42,32 @@ func TestCertificateManagerServiceCmd(t *testing.T) {
 	}
 	assert.NoError(t, err)
 
-	t.Run("cert list", func(t *testing.T) {
-		core.RootCmdTest.Command.SetArgs([]string{"list"})
-		err = core.RootCmdTest.Command.Execute()
-	})
+	t.Run(
+		"cert list", func(t *testing.T) {
+			viper.Reset()
+			// os.Clearenv()
+
+			viper.Set(constants.ArgOutput, constants.DefaultOutputFormat)
+			viper.Set(constants.ArgQuiet, true)
+			viper.Set(constants.ArgVerbose, false)
+
+			err = CertListCmd().Command.Execute()
+			assert.NoError(t, err)
+		},
+	)
+
+	t.Run(
+		"cert create", func(t *testing.T) {
+			viper.Reset()
+
+			viper.Set(constants.ArgOutput, constants.DefaultOutputFormat)
+			viper.Set(constants.ArgQuiet, false)
+			viper.Set(constants.ArgVerbose, false)
+
+
+			CertCreateCmd().Command.SetArgs([]string{"certificate-name", "cert_test"} )
+			CertCreateCmd().Command.Execute()
+			assert.NoError(t, err)
+		},
+	)
 }
