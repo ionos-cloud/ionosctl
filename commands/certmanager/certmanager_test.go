5d034b703e67128746855830fdb16520eec15b8d
dgheorghioiu-ionos <dragos.gheorghioiu@ionos.com>
2022-12-20 14:29:54 +0200

    certificate from file test

--- commands/certmanager/certmanager_test.go
+++ commands/certmanager/certmanager_test.go
@@ -10,6 +10,8 @@ import (
 	"encoding/pem"
 	"errors"
 	"math/big"
+	"os"
+	"path/filepath"
 	"testing"
 	"time"
 
@@ -91,7 +93,7 @@ func TestCertificateManagerServiceCmd(t *testing.T) {
 	)
 
 	t.Run(
-		"cert create", func(t *testing.T) {
+		"cert create from flags", func(t *testing.T) {
 			viper.Reset()
 
 			// os.Mkdir("testPaths", 777)
@@ -145,10 +147,90 @@ func TestCertificateManagerServiceCmd(t *testing.T) {
 				}
 			}
 
+			g := CertGetCmd()
+			g.Command.Flags().Set(FlagCertId, id)
+			assert.NoError(t, err)
 
 			d := CertDeleteCmd()
 			d.Command.Flags().Set(FlagCertId, id)
 			err = d.Command.Execute()
+			assert.NoError(t, err)
+		},
+	)
+
+	t.Run(
+		"cert create from files", func(t *testing.T) {
+			viper.Reset()
+
+			//os.Mkdir("./testPaths", 0777)
+			caPrivKey, err := rsa.GenerateKey(rand.Reader, 4096)
+			assert.NoError(t, err)
+
+			caBytes, err := x509.CreateCertificate(rand.Reader, ca, ca, &caPrivKey.PublicKey, caPrivKey)
+			assert.NoError(t, err)
+
+			caPEM := new(bytes.Buffer)
+			pem.Encode(caPEM, &pem.Block{
+				Type:  "CERTIFICATE",
+				Bytes: caBytes,
+			})
+
+			caPrivKeyPEM := new(bytes.Buffer)
+			pem.Encode(caPrivKeyPEM, &pem.Block{
+				Type:  "RSA PRIVATE KEY",
+				Bytes: x509.MarshalPKCS1PrivateKey(caPrivKey),
+			})
+			certPath := filepath.Join(".", "cert.pem")
+			//os.Create(certPath)
+			err = os.WriteFile(certPath, caPEM.Bytes(), 0777)
+			assert.NoError(t, err)
+
+			keyPath := filepath.Join(".", "key.pem")
+			//os.Create(keyPath)
+			os.WriteFile(keyPath, caPrivKeyPEM.Bytes(), 0777)
+			assert.NoError(t, err)
+
+			viper.Set(constants.ArgOutput, constants.DefaultOutputFormat)
+			viper.Set(constants.ArgQuiet, true)
+			viper.Set(constants.ArgVerbose, false)
+			viper.Set(constants.ArgForce, true)
+
+			c := CertCreateCmd()
+			c.Command.Flags().Set(FlagCertName, "certificate-files")
+			c.Command.Flags().Set(FlagCertPath, certPath)
+			c.Command.Flags().Set(FlagCertChainPath, certPath)
+			c.Command.Flags().Set(FlagPrivateKeyPath, keyPath)
+
+			err = c.Command.Execute()
+			assert.NoError(t, err)
+
+			// var id string
+			svc, err := resources.NewClientService(
+				viper.GetString(config.Username),
+				viper.GetString(config.Password),
+				viper.GetString(config.Token),
+				config.GetServerUrl(),
+			)
+			certs, _, err := svc.Get().CertificatesApi.CertificatesGet(context.Background()).
+				Execute()
+			assert.NoError(t, err)
+
+			var id string
+			for _, dto := range *certs.GetItems() {
+				if *dto.GetProperties().GetName() == "certificate-files" {
+					id = *dto.GetId()
+				}
+			}
+
+			d := CertDeleteCmd()
+			d.Command.Flags().Set(FlagCertId, id)
+			err = d.Command.Execute()
+			assert.NoError(t, err)
+
+			err = os.Remove(certPath)
+			assert.NoError(t, err)
+
+			err = os.Remove(keyPath)
 			assert.NoError(t, err)
 		},
 	)
