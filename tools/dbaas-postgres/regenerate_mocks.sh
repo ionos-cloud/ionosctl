27bd838f742c9c662f22bfb479a3fc231218d511
Alexandru Claudius Virtopeanu <alexandru-claudius.virtopeanu@ionos.com>
2023-02-13 12:06:09 +0200

    test: fix mocks

--- tools/dbaas-postgres/regenerate_mocks.sh
+++ tools/dbaas-postgres/regenerate_mocks.sh
@@ -7,7 +7,6 @@ pushd "services/dbaas-postgres/resources" >/dev/null || exit
 
 GO111MODULE=off go get -d github.com/golang/mock/mockgen
 
-mockgen -source client.go >mocks/ClientService.go
 mockgen -source backup.go >mocks/BackupService.go
 mockgen -source cluster.go >mocks/ClusterService.go
 mockgen -source version.go >mocks/VersionService.go
