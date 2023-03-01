e6112dc9e585050e90d3af1a7ae33e74c991c70c
Alexandru-Claudius Virtopeanu <alexandru-claudius.virtopeanu@ionos.com>
2023-03-01 17:12:01 +0200

    refactor: confirm pkg (#243)
    * refactor: add ask pkg
    
    * doc: added warning to deprecated func

---
+++ internal/confirm/confirm_test.go
@@ -0,0 +1,68 @@
+package confirm
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestFAsk(t *testing.T) {
+	tests := []struct {
+		name      string
+		input     string
+		overrides []bool
+		expected  bool
+	}{
+		{
+			name:      "Test empty input",
+			input:     "\n",
+			overrides: []bool{},
+			expected:  false,
+		},
+		{
+			name:      "Test yes input",
+			input:     "yes\n",
+			overrides: []bool{},
+			expected:  true,
+		},
+		{
+			name:      "Test no input",
+			input:     "no\n",
+			overrides: []bool{},
+			expected:  false,
+		},
+		{
+			name:      "Test uppercase yes input",
+			input:     "YES\n",
+			overrides: []bool{},
+			expected:  true,
+		},
+		{
+			name:      "Test uppercase no input",
+			input:     "NO\n",
+			overrides: []bool{},
+			expected:  false,
+		},
+		{
+			name:      "Test override true",
+			input:     "no\n",
+			overrides: []bool{true},
+			expected:  true,
+		},
+		{
+			name:      "Test multiple overrides",
+			input:     "\n",
+			overrides: []bool{false, true, false},
+			expected:  true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			in := bytes.NewBufferString(tt.input)
+			actual := FAsk("Test", in, tt.overrides...)
+			if actual != tt.expected {
+				t.Errorf("FAsk() = %v; want %v", actual, tt.expected)
+			}
+		})
+	}
+}
