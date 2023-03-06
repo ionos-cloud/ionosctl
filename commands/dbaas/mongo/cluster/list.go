a5a0a434769dbc30cef7cc112457df305df35397
Alexandru Claudius Virtopeanu <alexandru-claudius.virtopeanu@ionos.com>
2023-03-06 15:46:02 +0200

    temp: clusters pagination

--- commands/dbaas/mongo/cluster/list.go
+++ commands/dbaas/mongo/cluster/list.go
@@ -21,7 +21,17 @@ func ClusterListCmd() *core.Command {
 		CmdRun: func(c *core.CommandConfig) error {
 			c.Printer.Verbose("Getting Clusters...")
 			name := viper.GetString(core.GetFlagName(c.NS, constants.FlagName))
-			clusters, _, err := c.DbaasMongoServices.Clusters().List(name)
+			var limitPtr *int32 = nil
+			if f := core.GetFlagName(c.NS, constants.FlagMaxResults); viper.IsSet(f) {
+				limit := viper.GetInt32(f)
+				limitPtr = &limit
+			}
+			var offsetPtr *int32 = nil
+			if f := core.GetFlagName(c.NS, constants.FlagOffset); viper.IsSet(f) {
+				offset := viper.GetInt32(f)
+				offsetPtr = &offset
+			}
+			clusters, _, err := c.DbaasMongoServices.Clusters().List(name, limitPtr, offsetPtr)
 			if err != nil {
 				return err
 			}
