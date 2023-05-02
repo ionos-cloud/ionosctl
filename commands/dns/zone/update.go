12adc7984cd600d0427561d803ce5741727bdccc
Alexandru Claudius Virtopeanu <alexandru-claudius.virtopeanu@ionos.com>
2023-05-02 10:51:53 +0300

    fix: runnable

--- commands/dns/zone/update.go
+++ commands/dns/zone/update.go
@@ -13,21 +13,12 @@ func ZonesPutCmd() *core.Command {
 		Verb:      "update",
 		Aliases:   []string{},
 		ShortDesc: "Ensure a zone",
-		Example:   "ionosctl dns zoneupdate ",
+		Example:   "ionosctl dns zone update",
 		PreCmdRun: func(c *core.PreCommandConfig) error {
-			/* TODO: Delete/modify me for --all
-						 * err := core.CheckRequiredFlagsSets(c.Command, c.NS, []string{constants.ArgAll}, []string{constants.Flag<Parent>Id}, []string{constants.ArgAll, constants.Flag<Parent>Id})
-						 * if err != nil {
-						 * 	return err
-						 * }
-			             * */
-
-			// TODO: If no --all, mark individual flags as required
-
 			return nil
 		},
 		CmdRun: func(c *core.CommandConfig) error {
-			// Implement the actual command logic here
+			return nil
 		},
 		InitClient: true,
 	})
