3865bfb12c646b5c07c9c86a0ef6dabaa43cad10
Alexandru Claudius Virtopeanu <alexandru-claudius.virtopeanu@ionos.com>
2023-08-31 12:36:00 +0300

    feat: login logout

---
+++ commands/cfg/login.go
@@ -0,0 +1,178 @@
+package cfg
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/ionos-cloud/ionosctl/v6/internal/client"
+	"golang.org/x/term"
+
+	"github.com/ionos-cloud/ionosctl/v6/pkg/config"
+	"github.com/ionos-cloud/ionosctl/v6/pkg/constants"
+	"github.com/ionos-cloud/ionosctl/v6/pkg/core"
+	sdk "github.com/ionos-cloud/sdk-go/v6"
+	"github.com/spf13/viper"
+)
+
+func LoginCmd() *core.Command {
+
+	const (
+		loginFlagUseApiUrl = "use-default-api-url"
+
+		loginExamples = `ionosctl login --user $IONOS_USERNAME --password $IONOS_PASSWORD
+
+ionosctl login --token $IONOS_TOKEN
+
+ionosctl login
+Enter your username:
+USERNAME
+Enter your password:`
+	)
+
+	ctx := context.TODO()
+	loginCmd := core.NewCommand(ctx, nil, core.CommandBuilder{
+		Namespace: "login",
+		Resource:  "login",
+		Verb:      "login",
+		Aliases:   []string{"log", "auth"},
+		ShortDesc: "Use credentials to generate a config file in `ionosctl cfg location`",
+		LongDesc: fmt.Sprintf(`The 'login' command allows you to authenticate with the IONOS Cloud APIs. There are three ways you can use it:
+  1. Interactive mode: Just type 'ionosctl login' and you'll be prompted to enter your username and password.
+  2. Use the '--user' and '--password' flags: Enter your credentials in the command.
+  3. Use the '--token' flag: Provide an authentication token.
+
+If you use a username and password, this command generates a token that's saved in the config file. Please keep this token safe. If you specify a custom '--api-url', it'll be saved to the config file when you login successfully and used for future API calls.
+
+To find your config file location, use 'ionosctl cfg location'. If you want to use a different config file, use the '--config' global option. Changing the permissions of the config file will cause it to no longer work.
+
+Note: The IONOS Cloud CLI supports also authentication with environment variables: $IONOS_USERNAME, $IONOS_PASSWORD or $IONOS_TOKEN, these override the config file token.`),
+		Example:    loginExamples,
+		PreCmdRun:  PreRunLoginCmd,
+		CmdRun:     RunLoginUser,
+		InitClient: false,
+	})
+	loginCmd.AddStringFlag(constants.ArgUser, "", "", "Username to authenticate")
+	loginCmd.AddStringFlag(constants.ArgPassword, constants.ArgPasswordShort, "", "Password to authenticate")
+	loginCmd.AddStringFlag(constants.ArgToken, constants.ArgTokenShort, "", "Token to authenticate")
+	loginCmd.AddBoolFlag(loginFlagUseApiUrl, "", false, fmt.Sprintf(
+		"Use the default authentication URL (%s) for auth checking, even if you specify a different '--%s'", constants.DefaultApiURL, constants.ArgServerUrl))
+
+	return loginCmd
+}
+
+func PreRunLoginCmd(c *core.PreCommandConfig) error {
+	if (viper.IsSet(core.GetFlagName(c.NS, constants.ArgUser)) || viper.IsSet(core.GetFlagName(c.NS, constants.ArgPassword))) && viper.IsSet(core.GetFlagName(c.NS, constants.ArgToken)) {
+		return fmt.Errorf("use either --%s and/or --%s, either --%s", constants.ArgUser, constants.ArgPassword, constants.ArgToken)
+	}
+	return nil
+}
+
+func RunLoginUser(c *core.CommandConfig) error {
+	c.Printer.Verbose("Note: The login command will save the credentials in a configuration file after the authentication is successful!")
+	c.Printer.Verbose("Note: As an alternative to this, ionosctl offers support for environment variables: $%s, $%s or $%s.",
+		sdk.IonosUsernameEnvVar, sdk.IonosPasswordEnvVar, sdk.IonosTokenEnvVar)
+
+	data, err := buildConfigData(c)
+	if err != nil {
+		return fmt.Errorf("failed building config data: %w", err)
+	}
+
+	// Store credentials
+	c.Printer.Verbose("Storing credentials to the configuration file: %s", viper.GetString(constants.ArgConfig))
+	err = config.Write(data)
+	if err != nil {
+		return fmt.Errorf("failed writing config data: %w", err)
+	}
+
+	ls, _, err := client.Must().AuthClient.TokensApi.TokensGet(context.Background()).Execute()
+	if err != nil {
+		return err
+	}
+
+	// Go through data struct and blank out all credentials, including old ones (userdata.username, userdata.password)
+	msg := fmt.Sprintf("Authentication successful. Note: Your account has %d active tokens. Created the following fields in your config file:\n", len(*ls.Tokens))
+	for k, _ := range data {
+		msg += fmt.Sprintf(" • %s\n", strings.TrimPrefix(k, "userdata."))
+	}
+
+	_, err = fmt.Fprintln(c.Command.Command.OutOrStdout(), msg)
+	return err
+}
+
+// buildConfigData returns map that will be written to config file, while also checking token is usable via testToken
+func buildConfigData(c *core.CommandConfig) (map[string]string, error) {
+	configData := map[string]string{} // This map is what we will write to the config file
+
+	// API URL
+	c.Printer.Verbose("API Url: %s", config.GetServerUrl())
+	if explicitUrl := config.GetServerUrl(); strings.TrimLeft(explicitUrl, "https://") != strings.TrimLeft(constants.DefaultApiURL, "https://") {
+		// Don't save the API url to the config if it's the default, since we don't want to revert to that value if the user doesn't provide any.
+		// This was changed from old behaviour because some APIs (e.g. DNS API) [can] use a different server URL
+		c.Printer.Verbose("Saving API URL to config file")
+		configData[constants.CfgServerUrl] = explicitUrl
+	}
+
+	// Explicit token
+	token, err := c.Command.Command.Flags().GetString(constants.ArgToken)
+	if err != nil {
+		return nil, fmt.Errorf("failed reading token flag: %w", err)
+	}
+	if token != "" {
+		configData[constants.CfgToken] = token
+		return configData, client.TestCreds("", "", token) // Early return for preset token
+	}
+
+	// If here, user did not give a pre-set token. Generate one via username & password.
+	// If the username & password are not given via flags, ask for them interactively
+
+	var (
+		// Set by flags. If not set, interactive mode will ask the user iff token not set.
+		// They die here in this func block. Don't use them anywhere else!
+		username string
+		password string
+	)
+
+	// Note: Because of the prerun checks, we know for sure the following:
+	// Either flags for token set, or username set, or password set, or username & password set.
+	if fn := core.GetFlagName(c.NS, constants.ArgUser); viper.IsSet(fn) {
+		username = viper.GetString(fn)
+	} else {
+		// Interactively ask for username
+		_, err := fmt.Fprintln(c.Command.Command.OutOrStdout(), "Enter your username: ")
+		reader := bufio.NewReader(c.Command.Command.InOrStdin())
+		username, err = reader.ReadString('\n')
+		if err != nil {
+			return nil, fmt.Errorf("failed reading username from set reader")
+		}
+		username = strings.TrimSpace(username) // remove trailing newline
+	}
+
+	if fn := core.GetFlagName(c.NS, constants.ArgPassword); viper.IsSet(fn) {
+		password = viper.GetString(fn)
+	} else {
+		// Interactively ask for password
+		fmt.Fprintf(c.Command.Command.OutOrStdout(), "Enter your password: ")
+
+		if file, ok := c.Command.Command.InOrStdin().(*os.File); ok {
+			bytePassword, err := term.ReadPassword(int(file.Fd()))
+			if err != nil {
+				return nil, fmt.Errorf("failed securely reading password from set file descriptor")
+			}
+			password = string(bytePassword)
+		} else {
+			return nil, fmt.Errorf("the set input does not have a file descriptor (is it set to a terminal?)")
+		}
+	}
+
+	cl := client.NewClient(username, password, "", config.GetServerUrl())
+	tok, _, err := cl.AuthClient.TokensApi.TokensGenerate(context.Background()).Execute()
+	if err != nil {
+		return nil, fmt.Errorf("failed using username and password to generate a token: %w", err)
+	}
+
+	configData[constants.CfgToken] = *tok.Token
+	return configData, client.TestCreds("", "", *tok.Token)
+}
---
+++ commands/cfg/login_test.go
@@ -0,0 +1,273 @@
+//go:build integration
+// +build integration
+
+package cfg_test
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"os"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/cilium/fake"
+	"github.com/spf13/viper"
+
+	"github.com/ionos-cloud/ionosctl/v6/commands/cfg"
+
+	"github.com/ionos-cloud/ionosctl/v6/internal/client"
+	"github.com/ionos-cloud/ionosctl/v6/pkg/config"
+	"github.com/ionos-cloud/ionosctl/v6/pkg/constants"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	GoodUsername    = ""
+	GoodPassword    = ""
+	GoodToken       = ""
+	cl              *client.Client
+	tokCreationTime time.Time
+)
+
+func TestAuthCmds(t *testing.T) {
+	if err := setup(); err != nil {
+		t.Fatalf("Failed setting up auth tests: %s", err)
+	}
+	t.Cleanup(teardown)
+
+	assert.NotEmpty(t, GoodUsername)
+	assert.NotEmpty(t, GoodPassword)
+	assert.NotEmpty(t, GoodToken)
+
+	t.Parallel()
+
+	t.Run("login test user interactively, password as flag - valid token saved to config", func(t *testing.T) {
+		login := cfg.LoginCmd()
+		logout := cfg.LogoutCmd()
+
+		login.Command.Flags().Set(constants.ArgPassword, GoodPassword)
+
+		out := &bytes.Buffer{}
+		in := bytes.NewBufferString(GoodUsername + "\n")
+		login.Command.SetOut(out)
+		login.Command.SetIn(in)
+
+		// Exec login
+		err := login.Command.Execute()
+		assert.Contains(t, out.String(), "Enter your username: ")
+		assert.NoError(t, err)
+
+		// Read the configuration after login and assert that the token is valid
+		cfg, err := config.Read()
+		assert.NoError(t, err)
+
+		assert.NoError(t, client.TestCreds("", "", cfg[constants.CfgToken]))
+		assert.Empty(t, cfg[constants.ArgServerUrl])
+		assert.Empty(t, cfg[constants.CfgUsername])
+		assert.Empty(t, cfg[constants.CfgPassword])
+
+		// Exec logout
+		out = &bytes.Buffer{}
+		logout.Command.SetOut(out)
+		err = logout.Command.Execute()
+
+		// Assert that logout was successful and the token is removed from the configuration
+		assert.Contains(t, out.String(), "De-authentication successful")
+		assert.NoError(t, err)
+		cfg, err = config.Read()
+		assert.NoError(t, err)
+		assert.Empty(t, cfg[constants.CfgToken])
+	})
+
+	t.Run("login test user, password as flag - valid token saved to config", func(t *testing.T) {
+		login := cfg.LoginCmd()
+		logout := cfg.LogoutCmd()
+
+		login.Command.Flags().Set(constants.ArgUser, GoodUsername)
+		login.Command.Flags().Set(constants.ArgPassword, GoodPassword)
+
+		out := &bytes.Buffer{}
+		login.Command.SetOut(out)
+		err := login.Command.Execute()
+		assert.Contains(t, out.String(), "Authentication successful")
+		assert.NoError(t, err)
+
+		// Read the configuration after login and assert that the token is valid
+		cfg, err := config.Read()
+		assert.NoError(t, err)
+		assert.NoError(t, client.TestCreds("", "", cfg[constants.CfgToken]))
+
+		out = &bytes.Buffer{}
+		logout.Command.SetOut(out)
+		err = logout.Command.Execute()
+		assert.Contains(t, out.String(), "De-authentication successful")
+		assert.NoError(t, err)
+		cfg, err = config.Read()
+		assert.NoError(t, err)
+		assert.Empty(t, cfg[constants.CfgToken])
+	})
+
+	t.Run("valid user, password as flag - invalid token saved to config", func(t *testing.T) {
+		login := cfg.LoginCmd()
+		logout := cfg.LogoutCmd()
+
+		viper.Set(constants.ArgUser, fake.Adjective())
+		viper.Set(constants.ArgPassword, fake.Adjective())
+
+		config.Write(map[string]string{
+			constants.CfgToken: GoodToken,
+		})
+
+		// Read the configuration after login and assert that the token is valid
+		cfg, err := config.Read()
+		assert.NoError(t, err)
+		assert.NoError(t, client.TestCreds("", "", cfg[constants.CfgToken]))
+
+		out := &bytes.Buffer{}
+		login.Command.SetOut(out)
+		err = login.Command.Execute()
+		assert.Contains(t, out.String(), "Authentication successful")
+		assert.NoError(t, err)
+
+		out = &bytes.Buffer{}
+		logout.Command.SetOut(out)
+		err = logout.Command.Execute()
+		assert.Contains(t, out.String(), "De-authentication successful")
+		assert.NoError(t, err)
+		cfg, err = config.Read()
+		assert.NoError(t, err)
+		assert.Empty(t, cfg[constants.CfgToken])
+	})
+
+	t.Run("login test token as flag", func(t *testing.T) {
+		login := cfg.LoginCmd()
+		logout := cfg.LogoutCmd()
+
+		login.Command.Flags().Set(constants.ArgToken, GoodToken)
+
+		out := &bytes.Buffer{}
+		login.Command.SetOut(out)
+		err := login.Command.Execute()
+		assert.Contains(t, out.String(), "Authentication successful")
+		assert.NoError(t, err)
+
+		// Read the configuration after login and assert that the token is valid
+		cfg, err := config.Read()
+		assert.NoError(t, err)
+		assert.NoError(t, client.TestCreds("", "", cfg[constants.CfgToken]))
+
+		// In the case token is provided by user via --token ; then the saved cfg file token should be identical to the provided one
+		assert.Equal(t, cfg[constants.CfgToken], GoodToken)
+
+		out = &bytes.Buffer{}
+		logout.Command.SetOut(out)
+		err = logout.Command.Execute()
+
+		// Assert that logout was successful and the token is removed from the configuration
+		assert.Contains(t, out.String(), "De-authentication successful")
+		assert.NoError(t, err)
+		cfg, err = config.Read()
+		assert.NoError(t, err)
+		assert.Empty(t, cfg[constants.CfgToken])
+	})
+
+	t.Run("Pre-rework config file logout - Username and password removed from config file", func(t *testing.T) {
+		logout := cfg.LogoutCmd()
+
+		before := map[string]string{
+			constants.CfgUsername:  "UsernameHere",
+			constants.CfgPassword:  "PasswordHere",
+			constants.CfgToken:     "TokenHere",
+			constants.CfgServerUrl: "dont-kill-me.com",
+		}
+		config.Write(before)
+
+		// Read the configuration - is it what we expect ?
+		cfg, err := config.Read()
+		assert.NoError(t, err)
+		assert.Equal(t, before, cfg)
+
+		out := &bytes.Buffer{}
+		logout.Command.SetOut(out)
+		err = logout.Command.Execute()
+
+		// Assert that logout was successful and the username, password, token removed from cfg
+		assert.Contains(t, out.String(), "De-authentication successful")
+		assert.NoError(t, err)
+		after, err := config.Read()
+		assert.NoError(t, err)
+		assert.Empty(t, after[constants.CfgToken])
+		assert.Empty(t, after[constants.CfgPassword])
+		assert.Empty(t, after[constants.CfgUsername])
+		assert.Equal(t, before[constants.CfgServerUrl], after[constants.CfgServerUrl])
+	})
+
+	t.Run("cfg location cmd returns valid location", func(t *testing.T) {
+		cfgLocCmd := cfg.LocationCmd()
+		out := &bytes.Buffer{}
+		cfgLocCmd.Command.SetOut(out)
+		err := cfgLocCmd.Command.Execute()
+		assert.NoError(t, err)
+
+		assert.Equal(t, config.GetConfigFile(), out.String())
+
+	})
+}
+
+func setup() error {
+	GoodUsername = os.Getenv("IONOS_USERNAME")
+	GoodPassword = os.Getenv("IONOS_PASSWORD")
+
+	if GoodUsername == "" || GoodPassword == "" {
+		return fmt.Errorf("empty user/pass")
+	}
+
+	cl = client.NewClient(GoodUsername, GoodPassword, "", "")
+	tok, _, err := cl.AuthClient.TokensApi.TokensGenerate(context.Background()).Execute()
+
+	if err != nil {
+		return err
+	}
+
+	if tok.Token == nil {
+		return fmt.Errorf("tok is nil")
+	}
+
+	GoodToken = *tok.Token
+	tokCreationTime = time.Now().In(time.UTC).Add(-1 * time.Minute)
+
+	return nil
+}
+
+func teardown() {
+
+	toks, _, err := cl.AuthClient.TokensApi.TokensGet(context.Background()).Execute()
+
+	if err != nil {
+		panic(err)
+	}
+
+	// Delete tokens generated since setup
+	for _, t := range *toks.Tokens {
+		strDate, ok := strings.CutSuffix(*t.CreatedDate, "[UTC]")
+		if !ok {
+			panic("they changed the date format: no more [UTC] suffix")
+		}
+		date, err := time.Parse(time.RFC3339, strDate)
+		if err != nil {
+			panic(fmt.Errorf("they changed the date format: %w", err))
+		}
+
+		// Delete the token if it was created after setup
+		if date.After(tokCreationTime) {
+			_, _, err := cl.AuthClient.TokensApi.TokensDeleteById(context.Background(), *t.Id).Execute()
+
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
+
+}
