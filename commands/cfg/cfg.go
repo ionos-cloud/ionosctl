a59f134d40ded50854597d0a3ebae5dae2782097
Alexandru Claudius Virtopeanu <alexandru-claudius.virtopeanu@ionos.com>
2023-08-31 10:20:38 +0300

    feat: cfg command and add to root

---
+++ commands/cfg/cfg.go
@@ -0,0 +1,19 @@
+package cfg
+
+import (
+	"github.com/ionos-cloud/ionosctl/v6/pkg/core"
+	"github.com/spf13/cobra"
+)
+
+func ConfigCmd() *core.Command {
+	cmd := &core.Command{
+		Command: &cobra.Command{
+			Use:              "config",
+			Short:            "Manage your CLI Configuration",
+			Aliases:          []string{"cfg"},
+			TraverseChildren: true,
+		},
+	}
+	cmd.AddCommand(LocationCmd())
+	return cmd
+}
