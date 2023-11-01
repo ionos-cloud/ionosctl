3a0f8ffa67d29d88cc735bb5773947915403e6f1
Alexandru Claudius Virtopeanu <alexandru-claudius.virtopeanu@ionos.com>
2023-11-01 13:05:31 +0200

    refactor: reflect.go

--- internal/utils/reflect.go
+++ internal/utils/reflect.go
@@ -4,31 +4,44 @@ import (
 	"reflect"
 )
 
-func GetPropertiesKVSet(params ...interface{}) map[string]interface{} {
-	var argKind reflect.Kind
-	propertiesKVset := map[string]interface{}{}
-	for _, param := range params {
-		arg := reflect.TypeOf(param)
-		value := reflect.ValueOf(param)
-		if arg.Kind() == reflect.Ptr {
-			argKind = arg.Elem().Kind()
-		} else {
-			argKind = arg.Kind()
+// GetPropertiesKVSet converts a struct to a map[string]interface{}.
+// It will only include fields that are set and are not nil.
+// It will also recursively convert any nested structs.
+//
+//		type MyStruct struct {
+//		    Field1 int
+//		    Field2 string
+//		}
+//
+//	  instance := MyStruct{Field1: 42, Field2: "Hello"}
+//	  result := GetPropertiesKVSet(instance) // map[string]interface{}{"Field1": 42, "Field2": "Hello"}
+func GetPropertiesKVSet(i interface{}) map[string]interface{} {
+	properties := make(map[string]interface{})
+	if i == nil {
+		return properties
+	}
+
+	v := reflect.Indirect(reflect.ValueOf(i))
+	t := v.Type()
+
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		fieldValue := v.Field(i)
+
+		if fieldValue.Kind() == reflect.Ptr && !fieldValue.IsNil() {
+			fieldValue = fieldValue.Elem()
 		}
-		if argKind == reflect.Struct {
-			for i := 0; i < arg.NumField(); i++ {
-				if value.Field(i).Type().Kind() == reflect.Ptr {
-					if !value.Field(i).IsZero() && !value.Field(i).Elem().IsZero() {
-						if value.Field(i).Elem().Type().Kind() == reflect.Struct {
-							propertiesKVset[arg.Field(i).Name] = GetPropertiesKVSet(value.Field(i).Elem().Interface())
-						} else {
-							// Add Name of the field from struct and the value, if set
-							propertiesKVset[arg.Field(i).Name] = value.Field(i).Elem().Interface()
-						}
-					}
-				}
-			}
+
+		if fieldValue.IsZero() {
+			continue
+		}
+
+		// If the field is a struct, recursively call the function
+		if fieldValue.Kind() == reflect.Struct {
+			properties[field.Name] = GetPropertiesKVSet(fieldValue.Interface())
+		} else {
+			properties[field.Name] = fieldValue.Interface()
 		}
 	}
-	return propertiesKVset
+	return properties
 }
