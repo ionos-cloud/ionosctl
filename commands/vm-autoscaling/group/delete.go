a6a5e354b2131f6be29610f205175782f3e57278
Alexandru Claudius Virtopeanu <alexandru-claudius.virtopeanu@ionos.com>
2023-11-08 16:15:26 +0200

    feat: delete all groupts

--- commands/vm-autoscaling/group/delete.go
+++ commands/vm-autoscaling/group/delete.go
@@ -2,6 +2,7 @@ package group
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
 	"github.com/ionos-cloud/ionosctl/v6/internal/client"
@@ -9,6 +10,7 @@ import (
 	"github.com/ionos-cloud/ionosctl/v6/internal/core"
 	"github.com/ionos-cloud/ionosctl/v6/internal/printer/jsontabwriter"
 	"github.com/ionos-cloud/ionosctl/v6/internal/printer/tabheaders"
+	"github.com/ionos-cloud/ionosctl/v6/pkg/confirm"
 	vmasc "github.com/ionos-cloud/sdk-go-vm-autoscaling"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -30,6 +32,10 @@ func Delete() *core.Command {
 			)
 		},
 		CmdRun: func(c *core.CommandConfig) error {
+			if viper.GetBool(core.GetFlagName(c.NS, constants.ArgAll)) {
+				return deleteAll(c)
+			}
+
 			group, err := client.Must().VMAscClient.GroupsDelete(context.Background(),
 				viper.GetString(core.GetFlagName(c.NS, constants.FlagGroupId))).Execute()
 			if err != nil {
@@ -49,6 +55,7 @@ func Delete() *core.Command {
 		},
 	})
 
+	cmd.AddBoolFlag(constants.ArgAll, constants.ArgAllShort, false, "Set this flag to delete all VM-Autoscaling groups from your account")
 	cmd.AddStringFlag(constants.FlagGroupId, constants.FlagIdShort, "", "ID of the autoscaling group to list servers from")
 	_ = cmd.Command.RegisterFlagCompletionFunc(constants.FlagGroupId, func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
 		// get ID of all groups
@@ -58,4 +65,30 @@ func Delete() *core.Command {
 	})
 
 	return cmd
+}
+
+func deleteAll(c *core.CommandConfig) error {
+
+	var errs error
+	for _, id := range GroupsProperty(func(r vmasc.GroupResource) string {
+		return *r.Id
+	}) {
+		// find group by ID and ask the user if he is sure he wants to delete it
+		group, _, err := client.Must().VMAscClient.GroupsFindById(context.Background(), id).Execute()
+		if err != nil {
+			return fmt.Errorf("failed retrieving info about group %s: %w", id, err)
+		}
+
+		if !confirm.FAsk(c.Command.Command.InOrStdin(),
+			fmt.Sprintf("Do you really want to delete group %s from %s (%s)?", *group.Properties.Name, *group.Properties.Location, *group.Id),
+			viper.GetBool(constants.ArgForce)) {
+			errs = errors.Join(errs, fmt.Errorf("%s for %s", confirm.UserDenied, *group.Id))
+		}
+
+		_, err = client.Must().VMAscClient.GroupsDelete(context.Background(), id).Execute()
+		if err != nil {
+			return fmt.Errorf("failed deleting group %s: %w", id, err)
+		}
+
+	}
 }
