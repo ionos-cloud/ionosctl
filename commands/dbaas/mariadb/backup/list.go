44320f432798012ac7d6a6ed0c785ce3c7168feb
Alexandru Claudius Virtopeanu <alexandru-claudius.virtopeanu@ionos.com>
2024-02-28 14:36:09 +0200

    refactor: improve err

--- commands/dbaas/mariadb/backup/list.go
+++ commands/dbaas/mariadb/backup/list.go
@@ -6,7 +6,9 @@ import (
 
 	"github.com/ionos-cloud/ionosctl/v6/internal/constants"
 	"github.com/ionos-cloud/ionosctl/v6/internal/core"
+	"github.com/ionos-cloud/ionosctl/v6/internal/printer/json2table/jsonpaths"
 	"github.com/ionos-cloud/ionosctl/v6/internal/printer/jsontabwriter"
+	"github.com/ionos-cloud/ionosctl/v6/internal/printer/tabheaders"
 )
 
 func List() *core.Command {
@@ -27,15 +29,14 @@ func List() *core.Command {
 				return err
 			}
 
-			// cols, _ := c.Command.Command.Flags().GetStringSlice(constants.ArgCols)
+			cols, _ := c.Command.Command.Flags().GetStringSlice(constants.ArgCols)
 
-			// out, err := jsontabwriter.GenerateOutput("items", jsonpaths.DbaasPostgresBackup,
-			// 	backups, tabheaders.GetHeaders(allCols, defaultCols, cols))
-			// if err != nil {
-			// 	return err
-			// }
-			j, err := backups.MarshalJSON()
-			fmt.Fprintf(c.Command.Command.OutOrStdout(), string(j))
+			out, err := jsontabwriter.GenerateOutput("items", jsonpaths.DbaasPostgresBackup,
+				backups, tabheaders.GetHeaders(allCols, defaultCols, cols))
+			if err != nil {
+				return err
+			}
+			fmt.Fprintf(c.Command.Command.OutOrStdout(), out)
 			return nil
 		},
 		InitClient: true,
