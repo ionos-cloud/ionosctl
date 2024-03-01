c7316c9b900452820a91adfd048c50cc161af56b
Alexandru Claudius Virtopeanu <alexandru-claudius.virtopeanu@ionos.com>
2024-03-01 15:04:56 +0200

    go install should output a vers if installed via a tag

--- internal/version/version.go
+++ internal/version/version.go
@@ -2,6 +2,7 @@ package version
 
 import (
 	"fmt"
+	"regexp"
 	"runtime/debug"
 	"strings"
 )
@@ -23,14 +24,16 @@ func Get() string {
 	if ok {
 		versionParts := strings.Split(info.Main.Version, "-")
 		if len(versionParts) >= 3 {
-			commitHash := versionParts[2]
-			if len(commitHash) > 7 {
-				commitHash = commitHash[:7]
+			versionOrHash := versionParts[2]
+			if isSemanticVersion(versionOrHash) {
+				return versionOrHash
 			}
-			return fmt.Sprintf("DEV-%s", commitHash)
+			if len(versionOrHash) > 7 {
+				versionOrHash = versionOrHash[:7]
+			}
+			return fmt.Sprintf("DEV-%s", versionOrHash)
 		}
 	}
-
 	return "unknown"
 }
 
@@ -39,4 +42,10 @@ func getVersionViaLdFlags() string {
 		return "v" + strings.TrimLeft(Version, "v ")
 	}
 	return fmt.Sprintf("DEV-%s", GitCommit)
+}
+
+func isSemanticVersion(version string) bool {
+	semanticVersionPattern := `^v\d+\.\d+\.\d+$`
+	matched, _ := regexp.MatchString(semanticVersionPattern, version)
+	return matched
 }
