6fa2116ccb2e0e4ef2643b28923ec4e18e423610
Alexandru Claudius Virtopeanu <alexandru-claudius.virtopeanu@ionos.com>
2024-04-10 11:31:12 +0300

    revert: find and replace mishap

--- commands/dbaas/mongo/cluster/restore.go
+++ commands/dbaas/mongo/cluster/restore.go
@@ -16,7 +16,7 @@ import (
 
 func ClusterRestoreCmd() *core.Command {
 	cmd := core.NewCommand(context.TODO(), nil, core.CommandBuilder{
-		Namespace: "dbaas-mariadb",
+		Namespace: "dbaas-mongodb",
 		Resource:  "cluster",
 		Verb:      "restore",
 		Aliases:   []string{"r"},
