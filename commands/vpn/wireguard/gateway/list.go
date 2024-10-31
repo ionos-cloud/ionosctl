38564264e56a53ab523f304dd9b6f3d11ecf35f9
Alexandru Claudius Virtopeanu <alexandru-claudius.virtopeanu@ionos.com>
2024-10-31 13:35:20 +0200

    gateway list

--- commands/vpn/wireguard/gateway/list.go
+++ commands/vpn/wireguard/gateway/list.go
@@ -1,1 +1,65 @@
 package gateway
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/ionos-cloud/ionosctl/v6/internal/constants"
+	"github.com/ionos-cloud/ionosctl/v6/internal/core"
+	"github.com/ionos-cloud/ionosctl/v6/internal/printer/json2table/jsonpaths"
+	"github.com/ionos-cloud/ionosctl/v6/internal/printer/jsontabwriter"
+	"github.com/ionos-cloud/ionosctl/v6/internal/printer/tabheaders"
+	vpn "github.com/ionos-cloud/sdk-go-vpn"
+	"github.com/spf13/viper"
+)
+
+func List() *core.Command {
+	cmd := core.NewCommand(context.Background(), nil, core.CommandBuilder{
+		Namespace: "vpn",
+		Resource:  "wireguard gateway",
+		Verb:      "list",
+		Aliases:   []string{"l", "ls"},
+		ShortDesc: "List WireGuard Gateways",
+		Example:   "ionosctl vpn wireguard gateway list",
+		PreCmdRun: func(c *core.PreCommandConfig) error {
+			return nil
+		},
+		CmdRun: func(c *core.CommandConfig) error {
+			ls, err := Gateways(
+				func(req vpn.ApiWireguardgatewaysGetRequest) (vpn.ApiWireguardgatewaysGetRequest, error) {
+					if fn := core.GetFlagName(c.NS, constants.FlagOffset); viper.IsSet(fn) {
+						req = req.Offset(viper.GetInt32(fn))
+					}
+					if fn := core.GetFlagName(c.NS, constants.FlagMaxResults); viper.IsSet(fn) {
+						req = req.Limit(viper.GetInt32(fn))
+					}
+					return req, nil
+				},
+			)
+			if err != nil {
+				return fmt.Errorf("failed listing gateways: %w", err)
+			}
+
+			items, ok := ls.GetItemsOk()
+			if !ok || items == nil {
+				return fmt.Errorf("could not retrieve gateway items")
+			}
+
+			cols, _ := c.Command.Command.Flags().GetStringSlice(constants.ArgCols)
+			out, err := jsontabwriter.GenerateOutput(
+				"items",
+				jsonpaths.VPNWireguardGateway,
+				ls,
+				tabheaders.GetHeaders(allCols, defaultCols, cols),
+			)
+			if err != nil {
+				return err
+			}
+
+			fmt.Fprintf(c.Command.Command.OutOrStdout(), out)
+			return nil
+		},
+	})
+
+	return cmd
+}
