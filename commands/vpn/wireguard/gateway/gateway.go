517d1e54c1650add742d07f8754881d640db4405
Alexandru Claudius Virtopeanu <alexandru-claudius.virtopeanu@ionos.com>
2024-10-31 13:35:20 +0200

    gateway list

--- commands/vpn/wireguard/gateway/gateway.go
+++ commands/vpn/wireguard/gateway/gateway.go
@@ -1,10 +1,16 @@
 package gateway
 
 import (
+	"context"
+
+	"github.com/ionos-cloud/ionosctl/v6/internal/client"
 	"github.com/ionos-cloud/ionosctl/v6/internal/constants"
 	"github.com/ionos-cloud/ionosctl/v6/internal/core"
 	"github.com/ionos-cloud/ionosctl/v6/internal/printer/tabheaders"
+	"github.com/ionos-cloud/ionosctl/v6/pkg/functional"
 	"github.com/spf13/cobra"
+
+	vpn "github.com/ionos-cloud/sdk-go-vpn"
 )
 
 var (
@@ -29,6 +35,36 @@ func Root() *core.Command {
 	})
 
 	cmd.AddCommand(Create())
+	cmd.AddCommand(List())
 
 	return cmd
 }
+
+// GatewaysProperty returns a list of properties of all gateways matching the given filters
+func GatewaysProperty[V any](f func(gateway vpn.WireguardGatewayRead) V, fs ...Filter) []V {
+	recs, err := Gateways(fs...)
+	if err != nil {
+		return nil
+	}
+	return functional.Map(*recs.Items, f)
+}
+
+// Gateways returns all distributions matching the given filters
+func Gateways(fs ...Filter) (vpn.WireguardGatewayReadList, error) {
+	req := client.Must().VPNClient.WireguardGatewaysApi.WireguardgatewaysGet(context.Background())
+	for _, f := range fs {
+		var err error
+		req, err = f(req)
+		if err != nil {
+			return vpn.WireguardGatewayReadList{}, err
+		}
+	}
+
+	ls, _, err := req.Execute()
+	if err != nil {
+		return vpn.WireguardGatewayReadList{}, err
+	}
+	return ls, nil
+}
+
+type Filter func(request vpn.ApiWireguardgatewaysGetRequest) (vpn.ApiWireguardgatewaysGetRequest, error)
