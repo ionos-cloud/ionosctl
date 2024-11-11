141320d4adb2e07efd1d9186d930a6040eff8c95
Alexandru Claudius Virtopeanu <alexandru-claudius.virtopeanu@ionos.com>
2024-11-11 15:22:59 +0200

    location

--- commands/vpn/vpn.go
+++ commands/vpn/vpn.go
@@ -1,8 +1,14 @@
 package vpn
 
 import (
+	"fmt"
+	"maps"
+	"slices"
+
 	"github.com/ionos-cloud/ionosctl/v6/commands/vpn/wireguard"
+	"github.com/ionos-cloud/ionosctl/v6/internal/constants"
 	"github.com/ionos-cloud/ionosctl/v6/internal/core"
+	vpn "github.com/ionos-cloud/sdk-go-vpn"
 	"github.com/spf13/cobra"
 )
 
@@ -17,5 +23,30 @@ func Root() *core.Command {
 
 	cmd.AddCommand(wireguard.Root())
 
+	cmd.Command.PersistentFlags().String(constants.FlagLocation, "de/txl", fmt.Sprintf("The location your resources are hosted in. Possible values: %s", slices.Collect(maps.Keys(locationToURL))))
+	_ = cmd.Command.RegisterFlagCompletionFunc(constants.FlagLocation, func(c *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+		var locations []string
+		for k := range locationToURL {
+			locations = append(locations, k)
+		}
+
+		return locations, cobra.ShellCompDirectiveNoFileComp
+	})
+
 	return cmd
+}
+
+var locationToURL = map[string]string{
+	"de/fra": "https://vpn.de-fra.ionos.com",
+	"de/txl": "https://vpn.de-txl.ionos.com",
+}
+
+func changeLocation(client *vpn.APIClient, location string) {
+	cfg := client.GetConfig()
+	cfg.Servers = vpn.ServerConfigurations{
+		{
+			URL: locationToURL[location],
+		},
+	}
+	return
 }
