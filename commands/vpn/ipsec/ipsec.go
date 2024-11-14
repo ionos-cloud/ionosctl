54437d60a569e3263dfe03ef3a03e8864756eaa3
finicu212 <alexandru-claudius.virtopeanu@ionos.com>
2024-11-14 15:24:01 +0200

    add ipsec to vpn

--- commands/vpn/ipsec/ipsec.go
+++ commands/vpn/ipsec/ipsec.go
@@ -7,13 +7,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-/*
-IPSec is a modern VPN protocol that is lightweight, fast, and simple. It uses cryptographic primitives like Curve25519, ChaCha20, Poly1305, and BLAKE2s.
-A IPSec gateway establishes secure VPN connections using IPSec tunnels (which act as VPN endpoints).
-IPSec operates at the IP layer (Layer 3) and uses a stateless model with fewer moving parts.
-The configuration is static, meaning each tunnel (client or another gateway) is predefined in the configuration, which simplifies the overall setup.
-*/
-
 func Root() *core.Command {
 	cmd := &core.Command{
 		Command: &cobra.Command{
