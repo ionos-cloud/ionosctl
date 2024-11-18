9dc126793cbb5bd5fe1e4b868b6db5c8f831f7e9
finicu212 <alexandru-claudius.virtopeanu@ionos.com>
2024-11-18 17:15:07 +0200

    fix flags ipsec create gateway

--- internal/printer/json2table/jsonpaths/vpn.go
+++ internal/printer/json2table/jsonpaths/vpn.go
@@ -5,16 +5,13 @@ var VPNWireguardGateway = map[string]string{
 	"Name":           "properties.name",
 	"Description":    "properties.description",
 	"GatewayIP":      "properties.gatewayIP",
-	"InterfaceIPv4":  "properties.interfaceIPv4CIDR",
-	"InterfaceIPv6":  "properties.interfaceIPv6CIDR",
-	"ListenPort":     "properties.listenPort",
 	"DatacenterId":   "properties.connections[0].datacenterId",
 	"LanId":          "properties.connections[0].lanId",
 	"ConnectionIPv4": "properties.connections[0].ipv4CIDR",
 	"ConnectionIPv6": "properties.connections[0].ipv6CIDR",
 	"Status":         "metadata.status",
 	"StatusMessage":  "metadata.statusMessage",
-	"PublicKey":      "metadata.publicKey",
+	"Version":        "properties.version",
 }
 
 var VPNWireguardPeer = map[string]string{
