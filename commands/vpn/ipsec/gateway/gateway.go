da09fae47ba91c21176ccaf4225fc03f5b00e1db
finicu212 <alexandru-claudius.virtopeanu@ionos.com>
2024-11-18 17:21:30 +0200

    move completer

--- commands/vpn/ipsec/gateway/gateway.go
+++ commands/vpn/ipsec/gateway/gateway.go
@@ -1,24 +1,16 @@
 package gateway
 
 import (
-	"context"
-
-	"github.com/ionos-cloud/ionosctl/v6/internal/client"
-	"github.com/ionos-cloud/ionosctl/v6/internal/config"
 	"github.com/ionos-cloud/ionosctl/v6/internal/constants"
 	"github.com/ionos-cloud/ionosctl/v6/internal/core"
 	"github.com/ionos-cloud/ionosctl/v6/internal/printer/tabheaders"
-	"github.com/ionos-cloud/ionosctl/v6/pkg/functional"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
-
-	vpn "github.com/ionos-cloud/sdk-go-vpn"
 )
 
 var (
-	allCols = []string{"ID", "Name", "Description", "GatewayIP", "InterfaceIPv4", "InterfaceIPv6", "DatacenterId", "LanId", "ConnectionIPv4", "ConnectionIPv6", "InterfaceIP", "ListenPort", "Status"}
+	allCols = []string{"ID", "Name", "Description", "GatewayIP", "DatacenterId", "LanId", "ConnectionIPv4", "ConnectionIPv6", "Version", "Status"}
 	// we can safely include both InterfaceIPv4 and InterfaceIPv6 cols because if the respective column empty, it won't be shown
-	defaultCols = []string{"ID", "Name", "Description", "GatewayIP", "InterfaceIPv4", "InterfaceIPv6", "InterfaceIP", "ListenPort", "Status"}
+	defaultCols = []string{"ID", "Name", "Description", "GatewayIP", "Version", "Status"}
 )
 
 func Root() *core.Command {
@@ -44,36 +36,3 @@ func Root() *core.Command {
 
 	return cmd
 }
-
-// GatewaysProperty returns a list of properties of all gateways matching the given filters
-func GatewaysProperty[V any](f func(gateway vpn.IPSecGatewayRead) V, fs ...Filter) []V {
-	recs, err := Gateways(fs...)
-	if err != nil {
-		return nil
-	}
-	return functional.Map(*recs.Items, f)
-}
-
-// Gateways returns all distributions matching the given filters
-func Gateways(fs ...Filter) (vpn.IPSecGatewayReadList, error) {
-	if url := config.GetServerUrl(); url == constants.DefaultApiURL || url == "" {
-		viper.Set(constants.ArgServerUrl, constants.DefaultVPNApiURL)
-	}
-
-	req := client.Must().VPNClient.IPSecGatewaysApi.IpsecgatewaysGet(context.Background())
-	for _, f := range fs {
-		var err error
-		req, err = f(req)
-		if err != nil {
-			return vpn.IPSecGatewayReadList{}, err
-		}
-	}
-
-	ls, _, err := req.Execute()
-	if err != nil {
-		return vpn.IPSecGatewayReadList{}, err
-	}
-	return ls, nil
-}
-
-type Filter func(request vpn.ApiIpsecgatewaysGetRequest) (vpn.ApiIpsecgatewaysGetRequest, error)
