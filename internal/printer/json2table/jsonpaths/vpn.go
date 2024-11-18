33d64ace6ae4f75e0267d19c2840b8e6bbd1cd22
finicu212 <alexandru-claudius.virtopeanu@ionos.com>
2024-11-18 17:21:30 +0200

    move completer

--- internal/printer/json2table/jsonpaths/vpn.go
+++ internal/printer/json2table/jsonpaths/vpn.go
@@ -5,13 +5,16 @@ var VPNWireguardGateway = map[string]string{
 	"Name":           "properties.name",
 	"Description":    "properties.description",
 	"GatewayIP":      "properties.gatewayIP",
+	"InterfaceIPv4":  "properties.interfaceIPv4CIDR",
+	"InterfaceIPv6":  "properties.interfaceIPv6CIDR",
+	"ListenPort":     "properties.listenPort",
 	"DatacenterId":   "properties.connections[0].datacenterId",
 	"LanId":          "properties.connections[0].lanId",
 	"ConnectionIPv4": "properties.connections[0].ipv4CIDR",
 	"ConnectionIPv6": "properties.connections[0].ipv6CIDR",
 	"Status":         "metadata.status",
 	"StatusMessage":  "metadata.statusMessage",
-	"Version":        "properties.version",
+	"PublicKey":      "metadata.publicKey",
 }
 
 var VPNWireguardPeer = map[string]string{
