21b7adf6c52a04c8443980040e2354f9ebccf402
Alexandru Claudius Virtopeanu <alexandru-claudius.virtopeanu@ionos.com>
2024-11-29 15:56:46 +0200

    add comments, remove print statements

--- internal/core/flag-option.go
+++ internal/core/flag-option.go
@@ -40,6 +40,9 @@ func RequiredFlagOption() FlagOptionFunc {
 	}
 }
 
+// WithCompletionComplex is a FlagOptionFunc that allows for more complex completion logic.
+// It is recommended to use one of the simpler helper functions WithCompletion or WithCompletionE instead.
+// Only use this complex function if you need to handle more complex logic, like args-based completion, or custom filtering based on already typed keys (from toComplete)
 func WithCompletionComplex(
 	completionFunc func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective),
 	baseURL string,
@@ -58,18 +61,13 @@ func WithCompletionComplex(
 	return func(cmdToRegister *Command, flagName string) {
 		cmdToRegister.Command.RegisterFlagCompletionFunc(flagName,
 			func(passedCmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
-				fmt.Println("Completions For Flag: ", flagName)
-
 				viper.AutomaticEnv()
 
 				// Check if ArgServerURL is already set manually
 				if viper.IsSet(constants.ArgServerUrl) || viper.IsSet(constants.EnvServerUrl) {
-					fmt.Println("Server URL is set manually")
 					// If manually set, do nothing and directly call completionFunc
 					return completionFunc(passedCmd, args, toComplete)
 				}
-
-				fmt.Println("Server URL is not set manually")
 
 				// Handle location-based logic if allowedLocations is provided
 				if locationToURL != nil {
@@ -92,6 +90,13 @@ func WithCompletionComplex(
 	}
 }
 
+// WithCompletionE is a FlagOptionFunc that allows for a completion function that can return an error.
+//
+// Usage:
+//
+// - WithCompletionE(completionFuncE, "api.%s.ionos.com", allowedLocations) for a regional API
+//
+// - WithCompletionE(completionFuncE, "api.ionos.com", nil) for an API with a single endpoint
 func WithCompletionE(completionFunc func() ([]string, error), baseURL string, allowedLocations []string) FlagOptionFunc {
 	return WithCompletionComplex(func(_ *cobra.Command, _ []string, _ string) ([]string, cobra.ShellCompDirective) {
 		results, err := completionFunc()
@@ -102,6 +107,13 @@ func WithCompletionE(completionFunc func() ([]string, error), baseURL string, al
 	}, baseURL, allowedLocations)
 }
 
+// WithCompletion is a FlagOptionFunc that allows for a completion function that returns a list of strings.
+//
+// Usage:
+//
+// - WithCompletion(completionFunc, "api.%s.ionos.com", allowedLocations) for a regional API
+//
+// - WithCompletion(completionFunc, "api.ionos.com", nil) for an API with a single endpoint
 func WithCompletion(completionFunc func() []string, baseURL string, allowedLocations []string) FlagOptionFunc {
 	return WithCompletionComplex(func(_ *cobra.Command, _ []string, _ string) ([]string, cobra.ShellCompDirective) {
 		return completionFunc(), cobra.ShellCompDirectiveNoFileComp
