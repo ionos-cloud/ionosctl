82ec7f527ac7ba17217c4cbec077ffa8c598c6db
Alexandru Claudius Virtopeanu <alexandru-claudius.virtopeanu@ionos.com>
2025-01-13 22:44:45 +0200

    feat: add snapshot IDs to completion

--- commands/cloudapi-v6/volume.go
+++ commands/cloudapi-v6/volume.go
@@ -160,7 +160,7 @@ Required values to run command:
 	})
 	create.AddUUIDFlag(cloudapiv6.ArgImageId, "", "", "The Image Id or Snapshot Id to be used as template for the new Volume. A password or SSH Key need to be set")
 	_ = create.Command.RegisterFlagCompletionFunc(cloudapiv6.ArgImageId, func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
-		return completer.ImageIds(func(r ionoscloud.ApiImagesGetRequest) ionoscloud.ApiImagesGetRequest {
+		imageIds := completer.ImageIds(func(r ionoscloud.ApiImagesGetRequest) ionoscloud.ApiImagesGetRequest {
 			// Completer for HDD images that are in the same location as the datacenter
 			chosenDc, _, err := client.Must().CloudClient.DataCentersApi.DatacentersFindById(context.Background(),
 				viper.GetString(core.GetFlagName(create.NS, cloudapiv6.ArgDataCenterId))).Execute()
@@ -169,7 +169,11 @@ Required values to run command:
 			}
 
 			return r.Filter("location", *chosenDc.Properties.Location).Filter("imageType", "HDD")
-		}), cobra.ShellCompDirectiveNoFileComp
+		})
+
+		snapshotIds := completer.SnapshotIds()
+
+		return append(imageIds, snapshotIds...), cobra.ShellCompDirectiveNoFileComp
 	})
 
 	create.AddStringFlag(cloudapiv6.ArgImageAlias, cloudapiv6.ArgImageAliasShort, "", "The Image Alias to set instead of Image Id. A password or SSH Key need to be set")
