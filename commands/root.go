336eff720ecf5338e43e16ada11f793fb7ace672
Alexandru Claudius Virtopeanu <alexandru-claudius.virtopeanu@ionos.com>
2025-04-24 17:54:56 +0300

    feat: cfggen

--- commands/root.go
+++ commands/root.go
@@ -168,6 +168,7 @@ func addCommands() {
 	rootCmd.AddCommand(Shell())
 	rootCmd.AddCommand(VersionCmd())
 	rootCmd.AddCommand(Man())
+	rootCmd.AddCommand(GenCfgCmd())
 
 	// cfg
 	rootCmd.AddCommand(cfg.ConfigCmd())
