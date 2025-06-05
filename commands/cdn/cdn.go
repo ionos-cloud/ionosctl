61d48076dcf9727889b80e5cf6281f031961c11b
Alexandru Claudius Virtopeanu <alexandru-claudius.virtopeanu@ionos.com>
2025-06-05 13:45:27 +0300

    fix command WithRegionalFlags

--- commands/cdn/cdn.go
+++ commands/cdn/cdn.go
@@ -17,5 +17,5 @@ func Command() *core.Command {
 	}
 	cmd.AddCommand(distribution.Command())
 
-	return core.WithRegionalFlags(cmd, constants.CDNApiRegionalURL, constants.CDNLocations)
+	return core.WithRegionalFlags(cmd, "cdn", constants.CDNApiRegionalURL, constants.CDNLocations)
 }
