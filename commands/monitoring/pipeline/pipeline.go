16b25a2e6f1e37a33f2d8ffd096e55703a612930
cciopa-ionos <cristian-mihai.ciopa@ionos.com>
2025-07-08 15:30:20 +0300

    feat:add Monitoring Pipeline Get command (#538)
    * added findbyId and the completer

--- commands/monitoring/pipeline/pipeline.go
+++ commands/monitoring/pipeline/pipeline.go
@@ -28,6 +28,7 @@ func PipelineCommand() *core.Command {
 		},
 	)
 	cmd.AddCommand(MonitoringListCmd())
+	cmd.AddCommand(MonitoringFindByIdCmd())
 	cmd.AddCommand(MonitoringDeleteCmd())
 	cmd.AddCommand(MonitoringPostCmd())
 
