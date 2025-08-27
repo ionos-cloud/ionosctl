1927eeccb64a86916b9912c6367d8ea6509cb59e
Alexandru Claudius Virtopeanu <alexandru-claudius.virtopeanu@ionos.com>
2025-08-27 14:01:07 +0300

    refactor: replace service calls

--- commands/dbaas/postgres/waiter/deletion.go
+++ commands/dbaas/postgres/waiter/deletion.go
@@ -1,10 +1,14 @@
 package waiter
 
-import "github.com/ionos-cloud/ionosctl/v6/internal/core"
+import (
+	"context"
+
+	"github.com/ionos-cloud/ionosctl/v6/internal/client"
+	"github.com/ionos-cloud/ionosctl/v6/internal/core"
+)
 
 func ClusterDeleteInterrogator(c *core.CommandConfig, objId string) (*int, error) {
-	_, resp, err := c.CloudApiDbaasPgsqlServices.Clusters().Get(objId)
-	// Return HTTP Response Status Code
+	_, resp, err := client.Must().PostgresClient.ClustersApi.ClustersFindById(context.Background(), objId).Execute()
 	if resp != nil && resp.Response != nil {
 		return &resp.Response.StatusCode, nil
 	}
